3b104b3c614e6ed707f46d9d4ab54fb62c3bf5f5
nguyenthikimhue <nguyentkimhue13@dtu.edu.vn>
2025-04-01 20:04:06 +0700

    hr-emp management

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import React from "react";
 import SalaryManagement from "./components/PayrollManagement/SalaryManagement/SalaryManagement";
 import HRManagement from "./components/HRManager/HRManagement";
 import DashBoardPayroll from "./components/PayrollManagement/DashBoardPayroll/DashBoardPayroll";
+import HRDashboard from "./components/HRManager/HRDashboard/HRDashboard";
+import EmployeeManagement from "./components/HRManager/EmployeeManagement/EmployeeManagement";
 
 const App = () => {
   return (
@@ -13,7 +15,10 @@ const App = () => {
         <Route index element={<DashBoardPayroll />} />
         <Route path="/salary-management" element={<SalaryManagement />} />
       </Route>
-      <Route path="/HRManagement" element={<HRManagement />} />
+      <Route path="/HRManagement" element={<HRManagement />} >
+        <Route index element={<HRDashboard />} />
+        <Route path="/HRManagement/EmployeeManagement" element={<EmployeeManagement />} />
+      </Route>
     </Routes>
   );
 };
---
+++ src/components/HRManager/EmployeeManagement/EmployeeManagement.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EmployeeManagement = () => {
+  return (
+    <div>EmployeeManagement</div>
+  )
+}
+
+export default EmployeeManagement
