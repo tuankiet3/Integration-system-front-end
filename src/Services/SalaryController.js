a25709c5cc4909e57163f73562adc3732bb63e0c
main <quangff296@gmail.com>
2025-04-11 18:28:37 +0700

    salary history

--- src/Services/SalaryController.js
+++ src/Services/SalaryController.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://localhost:7280/api/",
+  baseURL: "https://localhost:7280/api",
 });
 
 const getSalary = async () => {
-  return instance.get("salaries");
+  return instance.get("/salaries");
 };
 
 export { getSalary };
